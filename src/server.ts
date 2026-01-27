9d17f7a1ebf6fd5dd979f303f07f33e7a6b82a97
Zabir Arkam Akhond <zabirarkam27@gmail.com>
2026-01-27 16:35:26 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 5000;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully.");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
